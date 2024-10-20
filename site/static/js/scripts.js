aae80c5addb6ef1caffc0701b0fc24951a0439a6
ArthurFunicap <arthur.00000849887@unicap.br>
2024-10-20 17:53:50 -0300

    commit

---
+++ site/static/js/scripts.js
@@ -0,0 +1,6 @@
+// Aqui você pode implementar validações futuras
+document.querySelector('.login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Lógica para login
+    console.log('Formulário enviado!');
+});
