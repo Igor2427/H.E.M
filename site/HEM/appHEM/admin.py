a1068ee8b5e6b55b0981c56f2c4aa3a5c82b5356
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2024-11-03 16:03:52 -0300

    Registrando os modelos em admin.py

--- site/HEM/appHEM/admin.py
+++ site/HEM/appHEM/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from appHEM.models import Medico
+from appHEM.models import Paciente
 
 # Register your models here.
+admin.site.register(Medico)
+admin.site.register(Paciente)
