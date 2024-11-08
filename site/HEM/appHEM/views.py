e875ec7d4ac9dbef5e9df35e0674a9621c93bbdc
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2024-11-08 07:57:22 -0300

    import httpResponse e loader

--- site/HEM/appHEM/views.py
+++ site/HEM/appHEM/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Medico, Paciente
 from django.contrib import messages
+from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
 def login(request):
