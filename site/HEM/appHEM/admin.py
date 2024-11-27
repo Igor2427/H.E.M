37e08ca3eab0b47b0716975de149d676690538fb
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2024-11-27 09:16:14 -0300

    Update admin.py

--- site/HEM/appHEM/admin.py
+++ site/HEM/appHEM/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from appHEM.models import Medico
 from appHEM.models import Paciente
 from appHEM.models import Favoritos
+from appHEM.models import Mensagem
 
 # Register your models here.
 admin.site.register(Medico)
 admin.site.register(Paciente)
 admin.site.register(Favoritos)
+admin.site.register(Mensagem)
